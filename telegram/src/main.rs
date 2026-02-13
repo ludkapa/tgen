9ef08e85650f67018426d6246635f66312d732c8
ludkapa <srneidezsr@gmail.com>
2026-02-14 00:42:29 +0700

    add a base code for telegramm teloxide webhook bot

--- telegram/src/main.rs
+++ telegram/src/main.rs
@@ -1,3 +1,78 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, error::Error, net::SocketAddr};
+
+use dotenvy::dotenv;
+use teloxide::{dispatching::dialogue::InMemStorage, prelude::*, update_listeners::webhooks};
+
+type HandlerResult = Result<(), Box<dyn Error + Sync + Send>>;
+
+//Dialogue State
+#[derive(Clone, Default)]
+enum DState {
+    #[default]
+    Start,
+    MainMenu,
+    Salary,
+    YearSelect {
+        salary: String,
+    },
+}
+
+#[tokio::main]
+async fn main() {
+    // Load logs
+    pretty_env_logger::init();
+    log::info!("Запуск бота...");
+    // Load envs
+    log::info!("Загрузка env...");
+    dotenv().ok();
+    let token = env::var("BOT_TOKEN").unwrap();
+    let port = env::var("PORT").unwrap();
+    let url = env::var("WEBHOOK_URL").unwrap();
+    run_bot(token, port, url).await;
+}
+
+async fn run_bot(token: String, port: String, webhook_url: String) {
+    // Init bot
+    let bot = Bot::new(token);
+    // Init ipv4 addr
+    let addr = SocketAddr::from(([0, 0, 0, 0], port.parse::<u16>().unwrap()));
+    // Setup webhook listener
+    let listener = webhooks::axum(
+        bot.clone(),
+        webhooks::Options::new(addr, webhook_url.parse().unwrap()),
+    )
+    .await
+    .expect("Не удалось поднять Webhook!");
+    // Dialogue update logic
+    let router = Update::filter_message()
+        .enter_dialogue::<Message, InMemStorage<DState>, DState>()
+        .branch(dptree::case![DState::Start].endpoint(start))
+        .branch(dptree::case![DState::MainMenu].endpoint(main_menu))
+        .branch(dptree::case![DState::Salary].endpoint(salary))
+        .branch(dptree::case![DState::YearSelect { salary }].endpoint(year_select));
+    // Dispatcher
+    Dispatcher::builder(bot, router)
+        .dependencies(dptree::deps![InMemStorage::<DState>::new()])
+        .enable_ctrlc_handler()
+        .build()
+        .dispatch_with_listener(
+            listener,
+            LoggingErrorHandler::with_custom_text("Ошибка при обновлении!"),
+        )
+        .await;
+    todo!()
+}
+
+async fn start() -> HandlerResult {
+    todo!()
+}
+
+async fn main_menu() -> HandlerResult {
+    todo!()
+}
+async fn salary() -> HandlerResult {
+    todo!()
+}
+async fn year_select() -> HandlerResult {
+    todo!()
 }
