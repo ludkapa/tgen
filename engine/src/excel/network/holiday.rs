eff583f6c46c366dab9950f486f765d063e51d2d
ludkapa <srneidezsr@gmail.com>
2026-02-16 16:55:46 +0700

    remake logic to get only lastest year dates

--- engine/src/excel/network/holiday.rs
+++ engine/src/excel/network/holiday.rs
@@ -8,37 +8,26 @@ use serde::Deserialize;
 #[derive(Deserialize, Debug, PartialEq)]
 pub struct FetchedDates {
     holidays: HashSet<NaiveDate>,
-    #[serde(skip)]
-    exist_years: Option<Vec<u16>>,
 }
 
 impl FetchedDates {
     pub async fn init() -> AResult<Self> {
         let url = "https://raw.githubusercontent.com/d10xa/holidays-calendar/refs/heads/master/json/calendar.json";
         let response = reqwest::get(url).await?.error_for_status()?;
-        let fetched_dates = match response.json::<FetchedDates>().await.ok() {
-            Some(dates) => dates,
-            None => return Err(anyhow::anyhow!("Cannot fetch holidays from github!")),
+        let mut fetched_dates = response.json::<FetchedDates>().await?;
+        let last_year = match fetched_dates.holidays.iter().map(|d| d.year()).max() {
+            Some(year) => year,
+            None => return Err(anyhow::anyhow!("Cannot get last year!")),
         };
+        fetched_dates.holidays = fetched_dates
+            .holidays
+            .into_iter()
+            .filter(|d| d.year() == last_year)
+            .collect();
         Ok(fetched_dates)
     }
 
-    pub(crate) fn for_year(&self, year: u16) -> HashSet<NaiveDate> {
-        self.holidays
-            .iter()
-            .filter(|d| d.year() == year as i32)
-            .copied()
-            .collect()
-    }
-
-    pub fn available_years(&mut self) -> Vec<u16> {
-        if let Some(cached) = &self.exist_years {
-            return cached.clone();
-        }
-        let mut years: Vec<u16> = self.holidays.iter().map(|d| d.year() as u16).collect();
-        years.sort();
-        years.dedup();
-        self.exist_years = Some(years.clone());
-        years
+    pub(crate) fn get_holidays(&self) -> HashSet<NaiveDate> {
+        self.holidays.clone()
     }
 }
