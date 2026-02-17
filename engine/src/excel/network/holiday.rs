f57c6e8ece7b300d5bf9cd83eaf2fdc8a08ed68d
ludkapa <srneidezsr@gmail.com>
2026-02-18 04:40:14 +0700

    added comments to holiday module

--- engine/src/excel/network/holiday.rs
+++ engine/src/excel/network/holiday.rs
@@ -12,18 +12,25 @@ pub struct FetchedDates {
 
 impl FetchedDates {
     pub async fn init() -> AResult<Self> {
+        // Url with holidays array in "year-month-day" format
         let url = "https://raw.githubusercontent.com/d10xa/holidays-calendar/refs/heads/master/json/calendar.json";
+        // Fetch data
         let response = reqwest::get(url).await?.error_for_status()?;
+        // Deserialize responce body into FetchedDates struct
         let mut fetched_dates = response.json::<FetchedDates>().await?;
+        // It will be optimized to current year fetching
+        // Try to find last year
         let last_year = match fetched_dates.holidays.iter().map(|d| d.year()).max() {
             Some(year) => year,
-            None => return Err(anyhow::anyhow!("Cannot get last year!")),
+            None => return Err(anyhow::anyhow!("Cannot find last year!")),
         };
+        // Get all holidays dates at finded year
         fetched_dates.holidays = fetched_dates
             .holidays
             .into_iter()
             .filter(|d| d.year() == last_year)
             .collect();
+        // Return
         Ok(fetched_dates)
     }
 
