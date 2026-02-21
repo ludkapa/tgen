636622d3ae0ffd20dd75396726d44034a5a98575
ludkapa <srneidezsr@gmail.com>
2026-02-22 00:30:15 +0700

    added empty strings before refactoring

--- tabel/src/excel/mod.rs
+++ tabel/src/excel/mod.rs
@@ -12,28 +12,38 @@ use rust_xlsxwriter::{Format, FormatBorder, utility::column_name_to_number, work
 pub async fn get_filled_table(salary: u32) -> AResult<Vec<u8>> {
     // Creating table
     let mut table = Workbook::new();
+
     // Fetch holidays
     let holidays = HolidayDates::init().await?;
+
     // Generate days for filling
     let days = Days::new_with_holidays(holidays.get_holidays());
+
     // Split days to chunks by month
     let chunks = days.split_months();
+
     for month_days in chunks {
         // Creating Sheet
         let month_worksheet = table.add_worksheet();
+
         // Make worksheet white
         month_worksheet.set_screen_gridlines(false);
+
         // For total formulas
         let mut weekend_cells = Vec::new();
         let mut usual_day_cells = Vec::new();
+
         // For work hours
         let mut work_hours: u16 = 0;
+
         // Iterate over days in month chunk
         for day in month_days {
             // Row number
             let row_number = 3 + day.number();
+
             // Adding day to month sheet and geting this flag
             let flag = add_day_cell(month_worksheet, day)?;
+
             // Creating formula for total block
             match flag {
                 DayType::Usual => {
@@ -52,19 +62,24 @@ pub async fn get_filled_table(salary: u32) -> AResult<Vec<u8>> {
         // Make formula string
         let weekends_formula = format!("={}", weekend_cells.join("+"));
         let usual_days_formula = format!("={}", usual_day_cells.join("+"));
+
         // Add salary
         let salary_str = match salary {
             0 => "".to_string(),
             _ => salary.to_string(),
         };
 
+        // format for salary cell
         let mut format = cell_style(DataType::Money, CellType::InputHeader);
+
+        // write salary cell
         month_worksheet.write_formula_with_format(
             4,
             column_name_to_number("E"),
             format!("={}", salary_str).as_str(),
             &format,
         )?;
+
         // Add a total block
         add_formula_cells(
             month_worksheet,
@@ -73,6 +88,7 @@ pub async fn get_filled_table(salary: u32) -> AResult<Vec<u8>> {
             usual_days_formula,
             weekends_formula,
         )?;
+
         // Polish worksheet
         // Do wider border at bottom of days block
         format = Format::new().set_border_top(FormatBorder::Medium);
@@ -84,14 +100,19 @@ pub async fn get_filled_table(salary: u32) -> AResult<Vec<u8>> {
             "",
             &format,
         )?;
+
         // Autofit columns
         month_worksheet.autofit();
+
         // Make E column wider
         month_worksheet.set_column_width(column_name_to_number("E"), 12)?;
+
         // Make C column wider
         month_worksheet.set_column_width(column_name_to_number("C"), 10)?;
+
         // Make B column narrower
         month_worksheet.set_column_width(column_name_to_number("B"), 7.5)?;
+
         // Set month name
         month_worksheet.set_name(month_days.first().unwrap().month_name())?;
     }
